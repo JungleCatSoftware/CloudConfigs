f4a6ab6d0c5d02097e3bdb4623a6a7500c34e50a
Bryan Wyatt <brwyatt@gmail.com>
2015-10-18 00:11:50 -0700

    Fix missing directories

--- example/modules/examplesite/manifests/init.pp
+++ example/modules/examplesite/manifests/init.pp
@@ -1,15 +1,28 @@
 class examplesite {
+
   include nginx
-  nginx::resource::vhost { 'example.net':
-    ensure   => present,
-    www_root => '/var/www/example.net',
-  }
+
+  file { '/var/www':
+    ensure => directory,
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0444',
+  } ->
+  file { '/var/www/example.net':
+    ensure => directory,
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0444',
+  } ->
   file { '/var/www/example.net/index.html':
     ensure  => file,
     owner   => 'www-data',
     group   => 'www-data',
     mode    => '0444',
     content => template('examplesite/index.html.erb'),
-    require => Nginx::Resource::Vhost['example.net'],
+  } ->
+  nginx::resource::vhost { 'example.net':
+    ensure   => present,
+    www_root => '/var/www/example.net',
   }
 }
